d103482b00e9c08bbfaa3ba7f9e42021d5123c34
code@awesomeash.com <code@awesomeash.com@ee4bc62e-8492-6f94-e013-73527c1846ef>
2012-05-31 06:59:38 +0000

    Initial Version

---
+++ trunk/userProfile.h
@@ -0,0 +1,42 @@
+String currentProfile = "Default";
+int profLight=200;
+float profTemp=35;
+
+
+/*************************************************
+  * Ashraf
+  *************************************************/
+
+const String ashID  = "8AF12856";
+const int ashLight = 500;
+const float ashTemp=20;
+
+/*************************************************
+  * Elvin
+  *************************************************/
+  
+const String elvID  = "DAE62756";
+const int elvLight = 300;
+  
+/*************************************************
+  * Koptiam
+  *************************************************/
+  
+const String kopID  = "32478B2D";
+const int kopLight = 800;
+
+/*************************************************
+  * Default
+  *************************************************/
+const int defaultLight = 0;
+
+
+const String tagID1 = "2DB4737C";
+const int id1Light = 250;
+
+const String tagID2 = "1DE7EC89";
+const int id2Light =  450;
+
+const String tagID3 = "7D78727C";
+const int id3Light = 500;
+
